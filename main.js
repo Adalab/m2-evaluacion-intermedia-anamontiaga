d785c8e0075959a588977283588be8b402bc5258
anamontiaga <amontiaga@gmail.com>
2019-07-29 15:54:14 +0200

    showing input value on console when click button

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 "use strict";
+
+// al pulsar el botón de "prueba", acceder al contenido del input y mostrarlo en la consola
+
+// recojo los elementos que voy a necesitar
+
+const btnEl = document.querySelector(".btn");
+const inputEl = document.querySelector(".input-number");
+const messageEl = document.querySelector(".page__message");
+
+// handler para recoger el número y mostrarlo en la consola
+function getImputValue() {
+  console.log(inputEl.value);
+}
+
+// listener al btn para que ejecute  getImputValue y showConsole
+btnEl.addEventListener("keyup", getImputValue);
